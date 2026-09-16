b4053d9b25b406c65e9aac45f0c497dc3a75cb8f
Flavin2212 <flavinantony123@gmail.com>
2026-09-16 10:44:54 +0530

    done

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter temperature in ");
+        Console.Write("Enter temperature in celsius ");
         double celsius = Convert.ToDouble(Console.ReadLine());
         double fahrenheit = (celsius * 9 /5) + 32;
         Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit);
